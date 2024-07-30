dd0280af8bac7da6db296b278b600ade68079945
Robbie <robbie331@>
2024-07-30 19:49:20 +0900

    Level1 - 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,5 +15,26 @@ public class App {
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
+
+        int result = 0;
+        switch (operator) {
+            case '+' :
+                result = first + second;
+                break;
+            case '-' :
+                result = first = second;
+                break;
+            case '*' :
+                result = first * second;
+                break;
+            case '/' :
+                if (second == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = first / second;
+                break;
+        }
+        System.out.println("결과: " + result);
     }
 }

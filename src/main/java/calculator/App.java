119557e22bb7df38d6898db37717ee204359e661
Robbie <robbie331@>
2024-07-30 19:39:32 +0900

    Level1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,17 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int first = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int second = sc.nextInt();
 
     }
 }
